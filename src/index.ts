c79af37476506e8d225fed3c5be6f281766ccacc
devsh <digimon.1740@gmail.com>
2018-05-24 23:25:08 +1000

    인터페이스

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-const sayHi = (name: string, age: number, gender: string): string=> {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name : string,
+    age : number,
+    gender : string
+}
+
+const person = {
+    name : "Tony",
+    age : 31,
+    gender : "male"
 };
 
-console.log(sayHi("Tony", 20, "male"));
+const sayHi = (person:Human): string=> {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
 
-console.log(1);
-console.log(12);
-console.log(132);
+console.log(sayHi(person));
+
+// const sayHi = (name: string, age: number, gender: string): string=> {
+//     return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// };
+//
+// console.log(sayHi("Tony", 20, "male"));
 
 export {};
