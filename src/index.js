b557cd4558fc0228a975d89d2ecc5cf73fb60d90
devsh <digimon.1740@gmail.com>
2018-05-24 22:58:09 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("Tony", 20, "male"));
+console.log(1);
+console.log(12);
+console.log(132);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const sayHi = (name: string, age: number, gender: string): string=> {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("Tony", 20, "male"));
+
+console.log(1);
+console.log(12);
+console.log(132);
+
+export {};
