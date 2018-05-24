40f9ffcffd030feee2a9dab7bd8b80e32ce84f49
devsh <digimon.1740@gmail.com>
2018-05-24 23:33:50 +1000

    클래스 예제

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
-interface Human {
-    name : string,
-    age : number,
-    gender : string
+// interface Human {
+//     name : string,
+//     age : number,
+//     gender : string
+// }
+//
+// const person = {
+//     name : "Tony",
+//     age : 31,
+//     gender : "male"
+// };
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    // ?는 옵셔널
+    constructor(name, age, gender?) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name : "Tony",
-    age : 31,
-    gender : "male"
-};
+const tony = new Human("tony", 30, "male");
 
-const sayHi = (person:Human): string=> {
+const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(tony));
 
 // const sayHi = (name: string, age: number, gender: string): string=> {
 //     return `Hello ${name}, you are ${age}, you are a ${gender}`;
